83167891a71a6220330e40e4a3e19a8be6f50e43
NastyVV <vip.nasty.tv@gmail.com>
2020-09-29 22:41:22 +0300

    очистка проекта от семпла, реализация меню

---
+++ src/components/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main-menu">
+    <div class="logo">
+
+    </div>
+    <div class="menu">
+      <ul>
+        <li><router-link to="/theory">Теория</router-link></li>
+        <li><router-link to="/practice">Практика</router-link></li>
+        <li><router-link to="/tests">Тесты</router-link></li>
+        <li><router-link to="/library">Медиатека</router-link></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Menu',
+
+}
+</script>
