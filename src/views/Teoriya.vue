407b4487d612312022d94a85aac5c5c362e29ad9
NastyVV <vip.nasty.tv@gmail.com>
2020-12-16 15:28:29 +0300

    main-menu

--- src/views/Teoriya.vue
+++ src/views/Teoriya.vue
@@ -5,6 +5,14 @@
         Теория
       </div>
     </div>
+    <div class="mainmenu-block">
+      <div class="test-block">1</div>
+      <div class="test-block">2</div>
+      <div class="test-block">3</div>
+      <div class="test-block">4</div>
+      <div class="test-block">5</div>
+      <div class="test-block">6</div>
+    </div>
   </div>
 </template>
 
