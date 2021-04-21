e1b1ff276744726adf09d4451d73080569fdc536
NastyVV <63959042+NastyVV@users.noreply.github.com>
2021-04-21 22:42:10 +0300

    rtet

---
+++ src/components/info-about.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="info-about-block">
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: "info-about"
+}
+</script>
+
